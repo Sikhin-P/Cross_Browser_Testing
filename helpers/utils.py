e7f6ad2a687a16561a925279876820b47558e488
Sikhin P <sikhin.parakkal@gofiber.com>
2023-12-26 15:44:18 +0530

    Chage in browsers

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,10 +1,10 @@
-from selenium.webdriver import Chrome, Edge, Firefox
+from selenium.webdriver import Chrome, Edge, Firefox, Ie
 
 
 def drivers(name):
     driver = {
         'Chrome': Chrome,
-        'Edge': Edge,
+        'Edge': Ie,
         'Firefox': Firefox
     }
     return driver[name]
